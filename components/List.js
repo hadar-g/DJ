62803e4a6c096e30b469797fa70eede7e669d7c7
Hadar Goldstein <hadargoldstein@Hadars-MacBook-Pro-16.local>
2023-03-12 15:20:08 -0400

    get change list on upvote working (not great algo change later)

--- components/List.js
+++ components/List.js
@@ -8,11 +8,36 @@ const List = (props) => {
     const [isFirstRun, setIsFirstRun] = useState(true)
     const[songsArray, setSongsArray] = useState([props.inputValue])
 
+    const reOrderArrayForUpvotes = (array, indexOf, indexIn, object) => {
+
+        const firstArray = array.slice(0, indexIn)
+        const secondArray = array.slice(indexIn, array.length)
+        const lastArray = firstArray.concat(object).concat(secondArray)
+        const firstNextArray = lastArray.slice(0,indexOf + 1)
+        const secondNextArray = lastArray.slice(indexOf + 2, array.length + 1)
+        const finalLastArray = firstNextArray.concat(secondNextArray)
+
+        return finalLastArray
+    }
+
     const onUpvotePressedHandler = (index) => {
-        console.log("Button pressed for item of index", index)
-        console.log(songsArray[index].title)
-        console.log(songsArray[index].votes)
-        setSongsArray(songsArray => songsArray.slice(0, index).concat({title: songsArray[index].title, votes: (songsArray[index].votes + 1)}).concat(songsArray.slice(index + 1, songsArray.length)))
+        const currentVotes = songsArray[index].votes + 1
+        const currentTitle = songsArray[index].title
+        let notFound = true
+        let loopValue = 0
+        while(notFound){   
+            if( currentVotes > songsArray[loopValue].votes){
+                notFound = false
+                continue
+            }
+            loopValue += 1
+        }
+       // console.log(loopValue)
+            setSongsArray(songsArray => reOrderArrayForUpvotes(songsArray, index, loopValue, {title: currentTitle, votes: currentVotes}))
+           // songsArray.splice(loopValue, 0, {title: currentTitle, votes: currentVotes})
+        
+        console.log(loopValue)
+     //   setSongsArray(songsArray => songsArray.slice(0, loopValue).concat({title: currentTitle, votes: currentVotes}).concat(songsArray.slice(loopValue + 1, songsArray.length)))
     }
     const onDownvotePressedHandler = (index) => {
         if(songsArray[index].votes > 0){
@@ -34,12 +59,12 @@ const List = (props) => {
         //setSongsArray(  songsArray.sort((a, b) => (a.votes > b.votes) ? 1 : -1))
     }, [props.inputValue])
 
-    useEffect(() => {
-        console.log("seconds use effect baby ")
+    // useEffect(() => {
+    //     console.log("seconds use effect baby ")
 
-        setSongsArray(songsArray => songsArray.sort((a, b) => (a.votes > b.votes) ? -1 : 1))
+    //     setSongsArray(songsArray => songsArray.sort((a, b) => (a.votes > b.votes) ? -1 : 1))
 
-    }, [onDownvotePressedHandler, onUpvotePressedHandler])
+    // }, [onDownvotePressedHandler, onUpvotePressedHandler])
 
 
     return(
