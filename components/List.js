27414db59ff52f725e93d2f9598cfd2540ad3e07
Hadar Goldstein <hadargoldstein@Hadars-MacBook-Pro-16.local>
2023-03-13 00:01:23 -0400

    clean up

--- components/List.js
+++ components/List.js
@@ -8,18 +8,18 @@ const List = (props) => {
     const [isFirstRun, setIsFirstRun] = useState(true)
     const[songsArray, setSongsArray] = useState([props.inputValue])
 
-    const reOrderArrayForUpvotes = (array, indexOf, indexIn, object) => {
+    const reOrderArrayForUpvotes = (array, indexOfItemToBeMoved, indexForItemToBePlaced, object) => {
 
-        const start = array.slice(0, indexIn)
-        const end = array.slice(indexIn, array.length)
+        const start = array.slice(0, indexForItemToBePlaced,)
+        const end = array.slice(indexForItemToBePlaced, array.length)
         const newArrayWithObject = start.concat(object).concat(end)
-        const newStart = newArrayWithObject.slice(0,indexOf + 1)
-        const newEnd = newArrayWithObject.slice(indexOf + 2, array.length + 1)
+        const newStart = newArrayWithObject.slice(0,indexOfItemToBeMoved + 1)
+        const newEnd = newArrayWithObject.slice(indexOfItemToBeMoved + 2, array.length + 1)
         const finalArray = newStart.concat(newEnd)
 
         return finalArray
     }
-    const noReOrderUpvote = (array, index, object) => {
+    const noReOrder = (array, index, object) => {
         const start = array.slice(0, index)
         const end = array.slice(index + 1, array.length)
         const finalArray = start.concat(object).concat(end)
@@ -34,18 +34,8 @@ const List = (props) => {
         const newEnd = arrayWith.slice(indexOfItemToBeMoved + 1, array.length + 1)
         const finalArray = newStart.concat(newEnd)
 
-        console.log("start: ", start)
-console.log("end: ", end)
-console.log("array with: " ,arrayWith)
-console.log("new start: ", newStart)
-console.log("new end: ", newEnd)
-console.log("new array: ", finalArray)
-
         return finalArray
         
-
-    }
-    const noReOrderDownvote = () => {
 
     }
 
@@ -62,7 +52,7 @@ console.log("new array: ", finalArray)
             loopValue += 1
         }
         if(loopValue == index){
-            setSongsArray(songsArray => noReOrderUpvote(songsArray, index, {title: currentTitle, votes: currentVotes}))
+            setSongsArray(songsArray => noReOrder(songsArray, index, {title: currentTitle, votes: currentVotes}))
             console.log("not moving up")
             return false
         }else{
@@ -70,40 +60,26 @@ console.log("new array: ", finalArray)
             setSongsArray(songsArray => reOrderArrayForUpvotes(songsArray, index, loopValue, {title: currentTitle, votes: currentVotes}))
             return true
         }
-       // console.log(loopValue)
-            
-           // songsArray.splice(loopValue, 0, {title: currentTitle, votes: currentVotes})
-        
-       // console.log(loopValue)
-     //   setSongsArray(songsArray => songsArray.slice(0, loopValue).concat({title: currentTitle, votes: currentVotes}).concat(songsArray.slice(loopValue + 1, songsArray.length)))
     }
     const onDownvotePressedHandler = (index) => {
-        console.log("downvote pressed in index", index)
         const currentVotes = songsArray[index].votes - 1
         const currentTitle = songsArray[index].title
-        let downNotFound = true
         let downLoopValue = songsArray.length
         
         while(downLoopValue != 0){  
             downLoopValue -= 1 
-           console.log('loop', downLoopValue)
-           console.log("index w" , index)
             if( currentVotes < songsArray[downLoopValue].votes){
-                //downNotFound = false
                 break
             }
         }
 
         if(downLoopValue == index){
-            setSongsArray(songsArray => noReOrderUpvote(songsArray, index, {title: currentTitle, votes: currentVotes}))
+            setSongsArray(songsArray => noReOrder(songsArray, index, {title: currentTitle, votes: currentVotes}))
             console.log("not moving down")
         }else{
             console.log("moving down")
             setSongsArray(songsArray => reOrderArrayForDownvotes(songsArray, downLoopValue, index, {title: currentTitle, votes: currentVotes}))
         }
-        // if(songsArray[index].votes > 0){
-        //     setSongsArray(songsArray => songsArray.slice(0, index).concat({title: songsArray[index].title, votes: (songsArray[index].votes - 1)}).concat(songsArray.slice(index + 1, songsArray.length)))
-        // }
         
     }
 
